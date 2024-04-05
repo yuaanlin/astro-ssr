99fb00131c34adca1962a0cca6382b992d538edc
EasonTang <eason@codeclassroom.com>
2024-04-05 21:33:58 +0800

    initialized

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import zeabur from "@zeabur/astro-adapter/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+    adapter: zeabur(),
+    output: "server",
+});
